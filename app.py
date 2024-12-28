04e1d9c3dff4d88364bb6e53c293582fdaefc7dd
Administrator <senthilshunmugam2003@gmail.com>
2024-12-28 10:59:57 +0000

    Update file app.py

--- app.py
+++ app.py
@@ -50,4 +50,4 @@ def predict():
     return jsonify({'predicted_text': predicted_text})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5500)
